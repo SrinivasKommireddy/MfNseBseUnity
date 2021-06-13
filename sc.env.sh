fe243a36ac708338f8c62f2c96dcca8bddbdad7d
SrinivasKommireddy <31505979+SrinivasKommireddy@users.noreply.github.com>
2021-06-13 21:08:05 +0530

    Update sc.env.sh

--- sc.env.sh
+++ sc.env.sh
@@ -13,12 +13,18 @@
   export AMC_LIST=mf.amc.txt
   export IDX_FILE="idx.ful.txt"
 #
-  export WGET="wget -nv -c -t0 -a wget.log --waitretry=20 -U Mozilla -4 "
-  #export WGETO_RWAIT="-w2 --random-wait --limit-rate=54k "
-  export WGETO_RWAIT="-w2 --random-wait "
+  #export WGET="wget -nv -c -t0 -a wget.log --waitretry=20 -U Mozilla -4 "
+  #export WGET_USER_AGENT='"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"'
+  #export WGET="wget -nv -c --tries=99 -a wget.log --waitretry=20 "
+  #export WGET="wget --verbose --continue --tries=99 --append-output=wget.log --timeout=90 --debug --waitretry=20 "
+  #export WGET_USER_AGENT="Mozilla/5.0 "
+  #export WGETO_RWAIT="-w2 --random-wait "
+
+  export WGET="wget --append-output=wget.log "
+
   export SORT="sort --parallel=4 --buffer-size=70% "
   #export THREADS=27
-  export THREADS=9
-  export BCKTRK_DAYS=3
+  export THREADS=1
+  export BCKTRK_DAYS=1
 #
   export PGPASSWORD="password"
